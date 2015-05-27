d57b1ff2c8e4911b0a94e6330952fd59574b7b86
wanghaogithub720 <wanghaomspace@gmail.com>
2015-05-27 18:50:40 +0800

    xxx

---
+++ RestaurantEvent/RestaurantEvent/business_layout/UIViewControllerStoryboardExtensions.swift
@@ -0,0 +1,11 @@
+//
+//  UIViewControllerStoryboardExtensions.swift
+//  RestaurantEvent
+//
+//  Created by djzhang on 5/27/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import Foundation
+
+
