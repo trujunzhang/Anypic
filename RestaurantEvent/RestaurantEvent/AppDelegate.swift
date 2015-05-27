8f71168ec1d2313484d73d2043f0ebb9f906a980
wanghaogithub720 <wanghaomspace@gmail.com>
2015-05-27 18:05:23 +0800

    xxx

--- RestaurantEvent/RestaurantEvent/AppDelegate.swift
+++ RestaurantEvent/RestaurantEvent/AppDelegate.swift
@@ -9,18 +9,21 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate,PAWLoginViewControllerDelegate {
+    
     var window: UIWindow?
-
-
+    lazy var navigationController:UINavigationController = {
+        return  UINavigationController(rootViewController:  UIViewController())
+        }()
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
+        
         // ****************************************************************************
         // Parse initialization
         ParseCrashReporting.enable()
         Parse.setApplicationId("cFOCgoE6v77JoeGXkF5cK5az4FLM5twfdOCGBphU", clientKey: "eJ5fVdx0SO8cXrZ8mTxxwREK34wpZ6VMcHBumKZl")
-
+        
         PFFacebookUtils.initializeFacebook()
         // ****************************************************************************
         
@@ -38,7 +41,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         defaultACL.setPublicReadAccess(true)
         
         PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
-
+        
+        
+        if ((PFUser.currentUser()) != nil) {
+            // Present wall straight-away
+            //            [self presentWallViewControllerAnimated:NO];
+        } else {
+            // Go to the welcome screen and have them log in or create an account.
+            self.presentLoginViewController()
+        }
         
         return true
     }
@@ -46,26 +57,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func presentTabBarController(){
         
     }
+    
 
+    // MARK: WallViewController
+    func presentWallViewControllerAnimated(animated: Bool){
+//        let wallViewController: PAWWallViewController = PAWWallViewController()
+//        wallViewController.delegate = self
+//        this.navigationController
+//        [self.navigationController setViewControllers:@[ wallViewController ] animated:animated];
+    }
+    
+    // MARK: PAWLoginViewControllerDelegate
+    func loginViewControllerDidLogin(controller: PAWLoginViewController!) {
+        //    [self presentWallViewControllerAnimated:YES];
+    }
+    
+    // MARK: LoginViewController
+    func presentLoginViewController() {
+        // Go to the welcome screen and have them log in or create an account.
+        let viewController: PAWLoginViewController
+        //        viewController.delegate = self
+        //        [self.navigationController setViewControllers:@[ viewController ] animated:NO];
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
@@ -73,7 +106,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
         return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication, withSession:PFFacebookUtils.session())
     }
-
-
+    
+    
 }
 
