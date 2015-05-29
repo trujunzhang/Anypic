fdfb9944d8dacff1e122366c60eabac7ac975a26
wanghaogithub720 <wanghaomspace@gmail.com>
2015-05-29 15:02:37 +0800

    add RestaurantEventBDD project.

---
+++ RestaurantEventBDD/RestaurantEventBDD/AppDelegate.swift
@@ -0,0 +1,104 @@
+//
+//  AppDelegate.swift
+//  RestaurantEventBDD
+//
+//  Created by djzhang on 5/29/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate,PAWLoginViewControllerDelegate {
+
+    var window: UIWindow?
+    lazy var navigationController:UINavigationController = {
+        return  UINavigationController(rootViewController:  UIViewController())
+        }()
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // ****************************************************************************
+        // Parse initialization
+        ParseCrashReporting.enable()
+        Parse.setApplicationId("cFOCgoE6v77JoeGXkF5cK5az4FLM5twfdOCGBphU", clientKey: "eJ5fVdx0SO8cXrZ8mTxxwREK34wpZ6VMcHBumKZl")
+        
+//        PFFacebookUtils.initializeFacebook()
+        // ****************************************************************************
+        
+        // Track app open.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        if (application.applicationIconBadgeNumber != 0) {
+            application.applicationIconBadgeNumber = 0;
+            PFInstallation.currentInstallation().saveInBackground()
+        }
+        
+        let defaultACL = PFACL();
+        
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.setPublicReadAccess(true)
+        
+        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        if ((PFUser.currentUser()) != nil) {
+            // Present wall straight-away
+             self.presentWallViewControllerAnimated(false)
+        } else {
+            // Go to the welcome screen and have them log in or create an account.
+            self.presentLoginViewController()
+            self.window?.rootViewController = self.navigationController
+        }
+        self.window?.makeKeyAndVisible()
+
+        
+        return true
+    }
+    
+    // MARK: WallViewController
+    func presentWallViewControllerAnimated(animated: Bool){
+        self.window?.backgroundColor = UIColor.redColor()
+    }
+    
+    // MARK: LoginViewController
+    func presentLoginViewController() {
+        // Go to the welcome screen and have them log in or create an account.
+        let viewController: PAWLoginViewController =  PAWLoginViewController(nibName: nil, bundle: nil)
+        viewController.delegate = self
+        self.navigationController.setViewControllers([viewController], animated: false)
+    }
+    
+    
+    // MARK: PAWLoginViewControllerDelegate
+    func loginViewControllerDidLogin(controller: PAWLoginViewController!) {
+         self.presentWallViewControllerAnimated(false)
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ RestaurantEventBDD/RestaurantEventBDD/RestaurantEventBDD-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// Our project object-c headers
+#import "RESources.h"
+
+
+// Library : parse and facebook
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+#import <FacebookSDK/facebookSDK.h>
+#import <ParseCrashReporting/ParseCrashReporting.h>
