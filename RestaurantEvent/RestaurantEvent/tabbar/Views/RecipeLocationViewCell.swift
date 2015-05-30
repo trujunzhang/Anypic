5ca7a9542187354ffc3cbe649992fbf0dc1e50f6
wanghaogithub720 <wanghaomspace@gmail.com>
2015-05-30 18:25:28 +0800

    xxx

---
+++ RestaurantEvent/RestaurantEvent/tabbar/Views/RecipeLocationViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeLocationViewCell.swift
+//  RestaurantEventBDD
+//
+//  Created by djzhang on 5/30/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class RecipeLocationViewCell: UITableViewCell {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RestaurantEvent/RestaurantEvent/tabbar/Views/UserHeaderViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  UserHeaderViewCell.swift
+//  RestaurantEventBDD
+//
+//  Created by djzhang on 5/30/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class UserHeaderViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
