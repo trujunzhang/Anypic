7a38f4cc172847f611769e08aca848ef9cca9a21
wanghaogithub720 <wanghaomspace@gmail.com>
2015-05-31 22:21:10 +0800

    xxx

--- RestaurantEvent/RestaurantEvent/tabbar/Views/RecipePhotoViewCell.swift
+++ RestaurantEvent/RestaurantEvent/tabbar/Views/RecipePhotoViewCell.swift
@@ -10,21 +10,42 @@ import UIKit
 
 class RecipePhotoViewCell: ParseAbstractTableCell {
 
-    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var photoContainer: UIView!
+    @IBOutlet weak var photoButton: UIButton!
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // Initialization code
+        self.opaque = false
+        self.selectionStyle = .None
+        self.accessoryType = .None
+
+        self.clipsToBounds = false
+        
+        self.backgroundColor = UIColor.clearColor()
+        
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
     // MARK: Setup table cell by object instanced PFObject
     override func setCell(object:PFObject){
-        let x = 0
+        
     }
 }
