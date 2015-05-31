157d406b2ddbfb56a080e1746a0549df32d1a1f4
wanghaogithub720 <wanghaomspace@gmail.com>
2015-05-31 22:37:29 +0800

    xxx

--- RestaurantEvent/RestaurantEvent/tabbar/Views/RecipePhotoViewCell.swift
+++ RestaurantEvent/RestaurantEvent/tabbar/Views/RecipePhotoViewCell.swift
@@ -9,33 +9,16 @@
 import UIKit
 
 class RecipePhotoViewCell: ParseAbstractTableCell {
-
+    
     @IBOutlet weak var photoContainer: UIView!
     @IBOutlet weak var photoButton: UIButton!
     
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        // Initialization code
-        self.opaque = false
-        self.selectionStyle = .None
-        self.accessoryType = .None
-
-        self.clipsToBounds = false
-        
-        self.backgroundColor = UIColor.clearColor()
-        
-        
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
+    lazy var photoView:PFImageView = { return PFImageView() }()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.setupTableCell()
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
@@ -43,6 +26,31 @@ class RecipePhotoViewCell: ParseAbstractTableCell {
         
         // Configure the view for the selected state
     }
+    
+    // MARK: Setup table cell
+    func setupTableCell(){
+        // Initialization code
+        self.opaque = false
+        self.selectionStyle = .None
+        self.accessoryType = .None
+        
+        self.clipsToBounds = false
+        
+        self.backgroundColor = UIColor.clearColor()
+        
+        // Initialization for PhotoView.
+        self.photoView.backgroundColor = UIColor.blackColor()
+        self.photoView.contentMode = .ScaleAspectFit
+        
+        self.photoContainer.addSubview(self.photoView)
+        self.photoView.LayoutFullView()
+        
+        self.photoView.image = UIImage(named: "PlaceholderPhoto.png")
+        
+        // Initialization for PhotoButton.
+        self.photoButton.backgroundColor = UIColor.clearColor()
+    }
+    
     
     // MARK: Setup table cell by object instanced PFObject
     override func setCell(object:PFObject){
