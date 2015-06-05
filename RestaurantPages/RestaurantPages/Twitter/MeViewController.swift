335913e8bcdca2d6432f907bb365733c3e3090fc
wanghaogithub720 <wanghaomspace@gmail.com>
2015-06-05 23:14:32 +0800

    xxx

---
+++ RestaurantPages/RestaurantPages/Twitter/MeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MeViewController.swift
+//  RestaurantPages
+//
+//  Created by djzhang on 6/5/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+    
+    @IBOutlet weak var infoContainerHeightConstraint: NSLayoutConstraint!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
---
+++ RestaurantPages/RestaurantPages/Twitter/TwitterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TwitterViewController.swift
+//  RestaurantPages
+//
+//  Created by djzhang on 6/5/15.
+//  Copyright (c) 2015 djzhang. All rights reserved.
+//
+
+import UIKit
+
+class TwitterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
